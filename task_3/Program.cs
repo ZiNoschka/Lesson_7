29485ce039fa41e1791c124abd68770a296a458d
Zina <zn26941@gmail.com>
2024-02-11 15:16:26 +0300

    added .task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] myArray = GenerateArray(-1, 157);
+
+        PrintReverse(myArray, myArray.Length - 1);
+    }
+
+    static void PrintReverse(int[] arr, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.WriteLine(arr[index]);
+        PrintReverse(arr, index - 1);
+    }
+
+    static int[] GenerateArray(int start, int end)
+    {
+        if (start > end)
+            return new int[0]; 
+
+        int[] arr = GenerateArray(start, end - 1); 
+        Array.Resize(ref arr, arr.Length + 1); 
+        arr[arr.Length - 1] = end; 
+        return arr;
+    }
+}
