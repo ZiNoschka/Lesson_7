d5136fae0e771f45849b55ac1022b2cde7b75651
Zina <zn26941@gmail.com>
2024-02-11 13:47:48 +0300

    added .task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static string GenerateNumberSequence(int M, int N)
+    {
+        if (M == N)
+            return M.ToString();
+    
+        else
+            return M + ", " + GenerateNumberSequence(M + 1, N);
+    }
+
+    static void Main(string[] args)
+    {
+        int M = 1; 
+        int N = 25; 
+
+        string result = GenerateNumberSequence(M, N);
+        Console.WriteLine(result); 
+    }
+}
